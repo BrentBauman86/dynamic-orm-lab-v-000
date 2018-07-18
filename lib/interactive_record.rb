ab88ef878012eb65b94cd5ac2ec47dd9a85def1a
Brent Bauman <brentbauman86@gmail.com>
2018-07-18 00:20:27 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -4,7 +4,6 @@ require 'active_support/inflector'
 class InteractiveRecord
 
   def self.table_name
-    # binding.pry
     self.to_s.downcase.pluralize
   end
 
@@ -57,5 +56,9 @@ end
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
+  end
+
+  def self.find_by 
+    sql = "SELECT * FROM #{self.attr_accessor}"
   end
 end
